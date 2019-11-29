4dab3ec8288050eb296da45b00aab0d0cd866087
Баранцев Степан <stepan.barantsev@gmail.com>
2019-11-29 17:02:31 +0300

    Create body of Caesar class

---
+++ crypto_algs/Caesar.py
@@ -0,0 +1,23 @@
+# Given a sequence of bytes. Add a few bits to each byte. Then we perform the operation mod 255
+class Caesar:
+
+    def __init__(self):
+        pass
+
+    def encrypt(self, sequence, shift):
+        pass
+
+    def decrypt(self, encrypted_sequence, shift):
+        pass
+
+    def decrypt_without_shift(self, encrypted_sequence):
+        pass
+
+    def encrypt_text(self, text, shift):
+        pass
+
+    def decrypt_text(self, text, shift):
+        pass
+
+    def decrypt_text_without_shift(self, text):
+        pass
