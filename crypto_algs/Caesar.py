b78b934fe2e0578f9fa83fda8c38c511c8907ad9
Баранцев Степан <stepan.barantsev@gmail.com>
2019-11-29 22:35:13 +0300

    Caesar (encrypt alg)

--- crypto_algs/Caesar.py
+++ crypto_algs/Caesar.py
@@ -1,11 +1,22 @@
-# Given a sequence of bytes. Add a few bits to each byte. Then we perform the operation mod 255
+from helpers.exceptions import CryptoException
+# Given a sequence of bytes. Add a few bits to each byte. Then we perform the operation mod 256
+# Also there are some text methods
+
+
 class Caesar:
 
     def __init__(self):
         pass
 
     def encrypt(self, sequence, shift):
-        pass
+        if type(sequence) != bytes:
+            raise TypeError('Type of sequence must be bytes')
+        if type(shift) != int:
+            raise TypeError('Type of shift must be integer')
+        # Convert to int array
+        sequence = [x for x in sequence]
+        new_sequence = [(element + shift) % 256 for element in sequence]
+        return bytearray(new_sequence)
 
     def decrypt(self, encrypted_sequence, shift):
         pass
