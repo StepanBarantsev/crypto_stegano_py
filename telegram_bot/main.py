7a362c883c8d840e5e60051ae680ac6ee516a23a
Баранцев Степан <stepan.barantsev@gmail.com>
2020-01-01 21:30:13 +0300

    Start realising tg bot

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -2,5 +2,18 @@ import telebot
 from settings import config
 from Caesar import Caesar
 
+bot = telebot.TeleBot(config["token"])
 
 
+@bot.message_handler(commands=['help'])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, '''Hello, this is help article''')
+
+
+@bot.message_handler(content_types=['document'])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, 'This is file to encrypt')
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
